a6fdcb5e784e7d56afe870680dba17cddac67f92
Peter Mai <peter.mai@mrjaffesclass.com>
2022-09-19 11:55:32 -0700

    initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello World!");
+        System.out.print("line 1");
+        System.out.print(" still line 1");
+    }
+}
